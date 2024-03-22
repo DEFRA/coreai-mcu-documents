bb1319d440c23876159c0cb5a48f7cca4df7e44c
BarryGeeDefra <barry.gee@defra.gov.uk>
2024-03-22 11:15:45 +0000

    Add POST route options

--- app/routes/document.js
+++ app/routes/document.js
@@ -1,14 +1,32 @@
 module.exports = [{
   method: 'POST',
   path: '/document',
-  handler: (request, h) => {
+  options: {
+    tags: ['api', 'document'],
+    payload: {
+      maxBytes: (50 * 1024 * 1024) + 250,
+      multipart: true,
+      timeout: false,
+      output: 'stream',
+      parse: true,
+      allow: 'multipart/form-data'
+    },
+  },
+  handler: async (request, h) => {
     console.log('Document POST endpoint hit')
-    return h.response('ok').code(200)
+
+    const { payload } = request
+    console.log('payload', payload)
+    return payload
+    // return h.response('ok').code(200)
   }
 },
 {
   method: 'PUT',
   path: '/document/{id}',
+  options: {
+    tags: ['api', 'document'],
+  },
   handler: (request, h) => {
     console.log(`Document PUT endpoint hit with id ${request.params.id}`)
     return h.response(`${request.params.id}`).code(200)
