fc95d6d1db51ea47cfd89e3c09fdcced841c23ca
barrygeedefra <barry.gee@defra.gov.uk>
2024-03-28 08:54:37 +0000

    Add getDocuments

--- app/storage/documents-repo.js
+++ app/storage/documents-repo.js
@@ -3,6 +3,40 @@ const { blobServiceClient } = require('./blob-service-client')
 const { mapMetadataToBlob } = require('../mappers/blob-metadata')
 const config = require('../config/storage')
 
+const getDocuments = async () => {
+  const documentsContainer = blobServiceClient.getContainerClient(config.documentsContainer)
+
+  let blobs = []
+
+  for await (const blob of documentsContainer.listBlobsFlat()) {
+    blobs.push(blob)
+  }
+
+  return blobs
+}
+
+// to do
+const getDocument = async (id) => {
+  const documentsContainer = blobServiceClient.getContainerClient(config.documentsContainer)
+  // const document = blobServiceClient.
+  return { id: `${id}` }
+}
+
+// to do
+const getDocumentMetadata = async (id) => {
+  // const documentMetaData = blobServiceClient.
+  return {
+    file_name: 'TestFile.pdf',
+    uploaded_by: 'TestUser',
+    document_type: 'Report',
+    source: 'Email',
+    source_address: 'test@example.com',
+    suggested_category: 'Finance',
+    user_category: 'Internal',
+    target_minister: 'Some Minister'
+  }
+}
+
 const saveDocument = async (buffer, type) => {
   const documentsContainer = blobServiceClient.getContainerClient(config.documentsContainer)
 
@@ -39,6 +73,9 @@ const updateDocumentMetadata = async (id, metadata) => {
 }
 
 module.exports = {
+  getDocuments,
+  getDocument,
+  getDocumentMetadata,
   saveDocument,
   updateDocumentMetadata
 }
