25b4e2075f6dc929fc6854c6984d8a089f08272b
steve-dickinson <steve.dickinson@defra.gov.uk>
2024-04-08 11:22:48 +0100

    Add initalise for document container

--- app/index.js
+++ app/index.js
@@ -1,10 +1,19 @@
 require('./insights').setup()
+const config = require('../config/storage')
 
 const createServer = require('./server')
+
+const initialiseContainers = async () => {
+  const documentsContainer = blobServiceClient.getContainerClient(config.documentsContainer)
+  console.log('Initialising containers...')
+  await documentsContainer.createIfNotExists()
+  console.log('Containers initialised')
+}
 
 const init = async () => {
   const server = await createServer()
   await server.start()
+  await initialiseContainers()
   console.log('Server running on %s', server.info.uri)
 }
 
